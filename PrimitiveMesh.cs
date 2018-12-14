8fc8e27b44e517f42113a60a500615f85177ec35
huwahuwa <42481055+huwahuwa2017@users.noreply.github.com>
2018-12-14 17:17:17 +0900

    Update PrimitiveMesh.cs

--- PrimitiveMesh.cs
+++ PrimitiveMesh.cs
@@ -1,17 +1,14 @@
 using System;
 using UnityEngine;
 
-namespace TestMod
+public class PrimitiveMesh
 {
-    public class PrimitiveMesh
-    {
-        private static Mesh[] AllPrimitiveMesh = new Mesh[Enum.GetValues(typeof(PrimitiveType)).Length];
+    private static Mesh[] AllPrimitiveMesh = new Mesh[Enum.GetValues(typeof(PrimitiveType)).Length];
 
-        public static Mesh Create(PrimitiveType PT)
-        {
-            int Index = (int)PT;
-            AllPrimitiveMesh[Index] = AllPrimitiveMesh[Index] ?? Resources.GetBuiltinResource<Mesh>(PT.ToString() + ".fbx");
-            return AllPrimitiveMesh[Index];
-        }
+    public static Mesh Create(PrimitiveType PT)
+    {
+        int Index = (int)PT;
+        AllPrimitiveMesh[Index] = AllPrimitiveMesh[Index] ?? Resources.GetBuiltinResource<Mesh>(PT.ToString() + ".fbx");
+        return AllPrimitiveMesh[Index];
     }
 }
