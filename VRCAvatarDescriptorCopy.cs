7e46b87009c19035f577b594bd2e708ade2de91e
huwahuwa <42481055+huwahuwa2017@users.noreply.github.com>
2022-12-21 10:56:28 +0900

    Update VRCAvatarDescriptorCopy.cs

--- VRCAvatarDescriptorCopy.cs
+++ VRCAvatarDescriptorCopy.cs
@@ -1,99 +1,295 @@
-﻿//2022/07/18
+//Ver3 2022/12/21 10:10
 
 #if UNITY_EDITOR
 
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Animations;
 using VRC.Core;
 using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Dynamics.PhysBone.Components;
 
 public class VRCAvatarDescriptorCopy : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _source;
+    private GameObject _sourceGameObject;
+    [SerializeField]
+    private bool _VRCPhysBoneCopy;
+    [SerializeField]
+    private bool _VRCPhysBoneColliderCopy;
+    [SerializeField]
+    private bool _constraintCopy;
 
-    private Transform[] _transformArray;
-
-    private SkinnedMeshRenderer[] _skinnedMeshRendererArray;
+    private IEnumerable<Transform> _dstTransformArray;
+    private IEnumerable<SkinnedMeshRenderer> _dstSkinnedMeshRendererArray;
 
     public void Copy()
     {
-        if (_source == null)
+        if (_sourceGameObject == null)
         {
             Debug.Log("コピー元となるゲームオブジェクトを設定してください");
             return;
         }
 
-        PipelineManager mpm = gameObject.GetComponent<PipelineManager>();
-        VRCAvatarDescriptor mad = gameObject.GetComponent<VRCAvatarDescriptor>();
-
-        mpm = mpm ?? gameObject.AddComponent<PipelineManager>();
-        mad = mad ?? gameObject.AddComponent<VRCAvatarDescriptor>();
-
-        string tpmj = JsonUtility.ToJson(_source.GetComponent<PipelineManager>());
-        string tadj = JsonUtility.ToJson(_source.GetComponent<VRCAvatarDescriptor>());
-
-        Debug.Log("PipelineManager : \n" + tpmj);
-        Debug.Log("VRCAvatarDescriptor : \n" + tadj);
-
-        JsonUtility.FromJsonOverwrite(tpmj, mpm);
-        JsonUtility.FromJsonOverwrite(tadj, mad);
+        _dstTransformArray = GetComponentsInChildren<Transform>(gameObject);
+        _dstSkinnedMeshRendererArray = GetComponentsInChildren<SkinnedMeshRenderer>(gameObject);
 
 
 
-        _transformArray = gameObject.GetComponentsInChildren<Transform>();
-        _skinnedMeshRendererArray = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+        Debug.Log("PipelineManagerとVRCAvatarDescriptorの複製開始");
 
-        TransformUpdate(mad.lipSyncJawBone, i => mad.lipSyncJawBone = i);
-        SkinnedMeshRendererUpdate(mad.VisemeSkinnedMesh, i => mad.VisemeSkinnedMesh = i);
+        PipelineManager mpm = ComponentCopyAndPaste(_sourceGameObject.GetComponent<PipelineManager>(), gameObject);
+        VRCAvatarDescriptor mad = ComponentCopyAndPaste(_sourceGameObject.GetComponent<VRCAvatarDescriptor>(), gameObject);
+
+        Debug.Log("PipelineManagerとVRCAvatarDescriptorの複製完了");
+
+
+
+        Debug.Log("VRCAvatarDescriptor内部のTransformの再割り当て開始");
+
+        mad.lipSyncJawBone = TransformSearch(mad.lipSyncJawBone);
+        mad.VisemeSkinnedMesh = SearchSkinnedMeshRenderer(mad.VisemeSkinnedMesh);
 
         VRCAvatarDescriptor.CustomEyeLookSettings cels = mad.customEyeLookSettings;
-        TransformUpdate(cels.leftEye, i => cels.leftEye = i);
-        TransformUpdate(cels.rightEye, i => cels.rightEye = i);
-        TransformUpdate(cels.upperLeftEyelid, i => cels.upperLeftEyelid = i);
-        TransformUpdate(cels.upperRightEyelid, i => cels.upperRightEyelid = i);
-        TransformUpdate(cels.lowerLeftEyelid, i => cels.lowerLeftEyelid = i);
-        TransformUpdate(cels.lowerRightEyelid, i => cels.lowerRightEyelid = i);
-        SkinnedMeshRendererUpdate(cels.eyelidsSkinnedMesh, i => cels.eyelidsSkinnedMesh = i);
+        cels.leftEye = TransformSearch(cels.leftEye);
+        cels.rightEye = TransformSearch(cels.rightEye);
+        cels.upperLeftEyelid = TransformSearch(cels.upperLeftEyelid);
+        cels.upperRightEyelid = TransformSearch(cels.upperRightEyelid);
+        cels.lowerLeftEyelid = TransformSearch(cels.lowerLeftEyelid);
+        cels.lowerRightEyelid = TransformSearch(cels.lowerRightEyelid);
+        cels.eyelidsSkinnedMesh = SearchSkinnedMeshRenderer(cels.eyelidsSkinnedMesh);
         mad.customEyeLookSettings = cels;
+
+        Debug.Log("VRCAvatarDescriptor内部のTransformの再割り当て完了");
+
+
+
+        if (_VRCPhysBoneCopy)
+        {
+            Debug.Log("VRCPhysBoneのコピーと再割り当ての開始");
+
+            List<VRCPhysBone> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<VRCPhysBone>(_sourceGameObject));
+
+            foreach (VRCPhysBone component in newComponents)
+            {
+                component.rootTransform = TransformSearch(component.rootTransform);
+            }
+
+            Debug.Log("VRCPhysBoneのコピーと再割り当ての完了");
+        }
+
+        if (_VRCPhysBoneColliderCopy)
+        {
+            Debug.Log("VRCPhysBoneColliderのコピーと再割り当ての開始");
+
+            List<VRCPhysBoneCollider> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<VRCPhysBoneCollider>(_sourceGameObject));
+
+            foreach (VRCPhysBoneCollider component in newComponents)
+            {
+                component.rootTransform = TransformSearch(component.rootTransform);
+            }
+
+            Debug.Log("VRCPhysBoneColliderのコピーと再割り当ての完了");
+        }
+
+        if (_constraintCopy)
+        {
+            {
+                Debug.Log("AimConstraintのコピーと再割り当ての開始");
+
+                List<AimConstraint> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<AimConstraint>(_sourceGameObject));
+
+                foreach (AimConstraint component in newComponents)
+                {
+                    component.worldUpObject = TransformSearch(component.worldUpObject);
+                    ConstraintSourcesTransformSearch(component);
+                }
+
+                Debug.Log("AimConstraintのコピーと再割り当ての完了");
+            }
+
+            {
+                Debug.Log("LookAtConstraintのコピーと再割り当ての開始");
+
+                List<LookAtConstraint> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<LookAtConstraint>(_sourceGameObject));
+
+                foreach (LookAtConstraint component in newComponents)
+                {
+                    component.worldUpObject = TransformSearch(component.worldUpObject);
+                    ConstraintSourcesTransformSearch(component);
+                }
+
+                Debug.Log("LookAtConstraintのコピーと再割り当ての完了");
+            }
+
+            {
+                Debug.Log("ParentConstraintのコピーと再割り当ての開始");
+
+                List<ParentConstraint> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<ParentConstraint>(_sourceGameObject));
+
+                foreach (ParentConstraint component in newComponents)
+                {
+                    ConstraintSourcesTransformSearch(component);
+                }
+
+                Debug.Log("ParentConstraintのコピーと再割り当ての完了");
+            }
+
+            {
+                Debug.Log("PositionConstraintのコピーと再割り当ての開始");
+
+                List<PositionConstraint> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<PositionConstraint>(_sourceGameObject));
+
+                foreach (PositionConstraint component in newComponents)
+                {
+                    ConstraintSourcesTransformSearch(component);
+                }
+
+                Debug.Log("PositionConstraintのコピーと再割り当ての完了");
+            }
+
+            {
+                Debug.Log("RotationConstraintのコピーと再割り当ての開始");
+
+                List<RotationConstraint> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<RotationConstraint>(_sourceGameObject));
+
+                foreach (RotationConstraint component in newComponents)
+                {
+                    ConstraintSourcesTransformSearch(component);
+                }
+
+                Debug.Log("RotationConstraintのコピーと再割り当ての完了");
+            }
+
+            {
+                Debug.Log("ScaleConstraintのコピーと再割り当ての開始");
+
+                List<ScaleConstraint> newComponents = ComponentsCopyAndPaste(GetComponentsInChildren<ScaleConstraint>(_sourceGameObject));
+
+                foreach (ScaleConstraint component in newComponents)
+                {
+                    ConstraintSourcesTransformSearch(component);
+                }
+
+                Debug.Log("ScaleConstraintのコピーと再割り当ての完了");
+            }
+        }
+
+
+
+        Debug.Log("すべての処理が完了しました");
     }
 
-    private void TransformUpdate(Transform target, Action<Transform> action)
+    private IEnumerable<T> GetComponentsInChildren<T>(GameObject gameObject) where T : Component
     {
-        if (target == null) return;
-        action(SearchTransform(target.name));
+        return gameObject.GetComponentsInChildren<T>().Where(c => gameObject != c.gameObject);
     }
 
-    private Transform SearchTransform(string search)
+    private T ComponentCopyAndPaste<T>(T srcComponent, GameObject dstGameObject) where T : Component
     {
-        Transform result = _transformArray.FirstOrDefault(i => i.name == search);
+        ComponentUtility.CopyComponent(srcComponent);
+
+        T newComponent = dstGameObject.GetComponent<T>();
+        string name = dstGameObject.transform.name;
+
+        if (newComponent == null)
+        {
+            ComponentUtility.PasteComponentAsNew(dstGameObject);
+            newComponent = dstGameObject.GetComponent<T>();
+
+            Debug.Log(name + "\nComponentを追加");
+        }
+        else
+        {
+            ComponentUtility.PasteComponentValues(newComponent);
+
+            Debug.Log(name + "\nComponentを上書き");
+        }
+
+        return newComponent;
+    }
+
+    private string GetPath(Transform transform)
+    {
+        string path = transform.name;
+        Transform parent = transform.parent;
+
+        while (parent.parent)
+        {
+            path = $"{parent.name}/{path}";
+            parent = parent.parent;
+        }
+
+        return path;
+    }
+
+    private Transform TransformSearch(Transform search)
+    {
+        if (search == null) return null;
+
+        string searchPass = GetPath(search);
+        Transform result = _dstTransformArray.FirstOrDefault(i => searchPass == GetPath(i));
 
         if (result == null)
         {
-            Debug.Log("Transform : " + search + "が見つかりません");
+            Debug.Log("SearchTransform\n" + searchPass + "が見つかりません");
+        }
+        else
+        {
+            Debug.Log("SearchTransform\n" + searchPass + "を取得");
         }
 
         return result;
     }
 
-    private void SkinnedMeshRendererUpdate(SkinnedMeshRenderer target, Action<SkinnedMeshRenderer> action)
+    private SkinnedMeshRenderer SearchSkinnedMeshRenderer(SkinnedMeshRenderer search)
     {
-        if (target == null) return;
-        action(SearchSkinnedMeshRenderer(target.name));
-    }
+        if (search == null) return null;
 
-    private SkinnedMeshRenderer SearchSkinnedMeshRenderer(string search)
-    {
-        SkinnedMeshRenderer result = _skinnedMeshRendererArray.FirstOrDefault(i => i.name == search);
+        string searchPass = GetPath(search.transform);
+        SkinnedMeshRenderer result = _dstSkinnedMeshRendererArray.FirstOrDefault(i => searchPass == GetPath(i.transform));
 
         if (result == null)
         {
-            Debug.Log("SkinnedMeshRenderer : " + search + "が見つかりません");
+            Debug.Log("SearchSkinnedMeshRenderer\n" + searchPass + "が見つかりません");
         }
 
         return result;
+    }
+
+    private List<T> ComponentsCopyAndPaste<T>(IEnumerable<T> components) where T : Component
+    {
+        List<T> newComponents = new List<T>();
+
+        foreach (T component in components)
+        {
+            Transform targetTransform = TransformSearch(component.transform);
+            if (targetTransform == null) continue;
+
+            newComponents.Add(ComponentCopyAndPaste(component, targetTransform.gameObject));
+        }
+
+        return newComponents;
+    }
+
+    private void ConstraintSourcesTransformSearch(IConstraint constraint)
+    {
+        List<ConstraintSource> constraintSources = new List<ConstraintSource>();
+        constraint.GetSources(constraintSources);
+
+        List<ConstraintSource> newConstraintSources = new List<ConstraintSource>();
+
+        foreach (ConstraintSource constraintSource in constraintSources)
+        {
+            ConstraintSource temp = constraintSource;
+            temp.sourceTransform = TransformSearch(temp.sourceTransform);
+            newConstraintSources.Add(temp);
+        }
+
+        constraint.SetSources(newConstraintSources);
     }
 }
 
@@ -102,19 +298,28 @@ public class VRCAvatarDescriptorCopyEditor : Editor
 {
     private VRCAvatarDescriptorCopy _instance;
 
-    private SerializedProperty _sourceProp;
+    private SerializedProperty _sourceGameObjectProp;
+    private SerializedProperty _VRCPhysBoneCopyProp;
+    private SerializedProperty _VRCPhysBoneColliderCopyProp;
+    private SerializedProperty _constraintCopyProp;
 
     private void OnEnable()
     {
         _instance = target as VRCAvatarDescriptorCopy;
 
-        _sourceProp = serializedObject.FindProperty("_source");
+        _sourceGameObjectProp = serializedObject.FindProperty("_sourceGameObject");
+        _VRCPhysBoneCopyProp = serializedObject.FindProperty("_VRCPhysBoneCopy");
+        _VRCPhysBoneColliderCopyProp = serializedObject.FindProperty("_VRCPhysBoneColliderCopy");
+        _constraintCopyProp = serializedObject.FindProperty("_constraintCopy");
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        EditorGUILayout.PropertyField(_sourceProp);
+        EditorGUILayout.PropertyField(_sourceGameObjectProp);
+        EditorGUILayout.PropertyField(_VRCPhysBoneCopyProp);
+        EditorGUILayout.PropertyField(_VRCPhysBoneColliderCopyProp);
+        EditorGUILayout.PropertyField(_constraintCopyProp);
         serializedObject.ApplyModifiedProperties();
 
         GUILayout.Space(10f);
