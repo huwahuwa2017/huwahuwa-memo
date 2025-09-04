1431a022ec1ace8fd4f596ab64ea09566bdd5b2a
huwahuwa2017 <huwahuwa314@gmail.com>
2025-09-04 16:56:22 +0900

    VRS Update

--- OldVirtualRealitySupport.cs
+++ OldVirtualRealitySupport.cs
@@ -1,26 +1,28 @@
-// v3
+// v5 2025-09-04 16:54
+
+// https://huwahuwa2017.hatenablog.com/entry/2024/06/02/015207
 
 #if UNITY_EDITOR
 
-using UnityEditor;
-
-public class VRS
-{
 #pragma warning disable 618
 
-    [MenuItem("Tools/VR Supported Off", false, 0)]
+using UnityEditor;
+
+public class OldVirtualRealitySupport
+{
+    [MenuItem("Tools/Old VR Support Off", false, 0)]
     static private void VRS_Off()
     {
         PlayerSettings.virtualRealitySupported = false;
     }
 
-    [MenuItem("Tools/VR Supported On", false, 0)]
+    [MenuItem("Tools/Old VR Support On", false, 0)]
     static private void VRS_On()
     {
         PlayerSettings.virtualRealitySupported = true;
     }
+}
 
 #pragma warning restore 618
-}
 
 #endif
