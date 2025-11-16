25be4df47e875767e3c5133de2911bf6581e8749
huwahuwa2017 <huwahuwa314@gmail.com>
2025-11-17 00:38:32 +0900

    Create RemoveAllMissingScript.cs

---
+++ RemoveAllMissingScript.cs
@@ -0,0 +1,25 @@
+﻿// v2 2025-11-17 00:37
+
+#if UNITY_EDITOR
+
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class RemoveAllMissingScript
+{
+    [MenuItem("Tools/RemoveAllMissingScript")]
+    private static void Test()
+    {
+        GameObject[] rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+        Transform[] temp0 = rootGameObjects.SelectMany(i => i.GetComponentsInChildren<Transform>(true)).ToArray();
+
+        foreach (Transform t in temp0)
+        {
+            GameObjectUtility.RemoveMonoBehavioursWithMissingScript(t.gameObject);
+        }
+    }
+}
+
+#endif
