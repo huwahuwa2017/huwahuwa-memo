ba9e60da0dbc09a1ad11d54b475c00eac329cde3
huwahuwa2017 <huwahuwa314@gmail.com>
2026-05-19 17:46:29 +0900

    Create BoundingBoxEditor.cs

---
+++ BoundingBoxEditor.cs
@@ -0,0 +1,49 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+
+public class BoundingBoxEditor : MonoBehaviour
+{
+    [SerializeField]
+    private Mesh _mesh = null;
+
+    [SerializeField]
+    private Vector3 _center = default;
+
+    [SerializeField]
+    private Vector3 _size = Vector3.one;
+
+    [ContextMenu("CheckBounds")]
+    private void CheckBounds()
+    {
+        Bounds b = _mesh.bounds;
+        Vector3 c = b.center;
+        Vector3 s = b.size;
+
+        Debug.Log($"center : {c.x}, {c.y}, {c.z}\nsize : {s.x}, {s.y}, {s.z}");
+    }
+
+    private void CreateMesh(Mesh mesh)
+    {
+        UnityEditor.AssetDatabase.CreateAsset(mesh, "Assets/BBE_Result.asset");
+        UnityEditor.AssetDatabase.SaveAssets();
+    }
+
+    [ContextMenu("RecalculateBounds")]
+    private void RecalculateBounds()
+    {
+        Mesh mesh = Instantiate(_mesh);
+        mesh.RecalculateBounds();
+        CreateMesh(mesh);
+    }
+
+    [ContextMenu("EditBounds")]
+    private void EditBounds()
+    {
+        Mesh mesh = Instantiate(_mesh);
+        mesh.bounds = new Bounds(_center, _size);
+        CreateMesh(mesh);
+    }
+}
+
+#endif
